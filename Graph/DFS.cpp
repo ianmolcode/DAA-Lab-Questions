5dcb667250617f8f0ca492b36ae3ce9c31baf46e
Anmol Narang <nanmol0406@gmail.com>
2022-05-15 15:27:30 +0530

    updated

---
+++ Graph/DFS.cpp
@@ -0,0 +1,24 @@
+// function
+
+void DFS_Final(vector<int> adj[], int s, bool visited[])
+{
+    visited[s] = true;
+    cout << s << " ";
+    for (int u; adj[s])
+    {
+        if (visited == false)
+        {
+            visited[u] == true;
+            DFS_Final(adj, u, visited);
+        }
+    }
+}
+void DFS(vector<int> adj, int v, int s)
+{
+    vector<bool> vec(v, false);
+    // or
+    bool visited[v];
+    for (int i = 0; i < v; i++)
+        visited[v] = false;
+    DFS_Final(adj, s);
+}
