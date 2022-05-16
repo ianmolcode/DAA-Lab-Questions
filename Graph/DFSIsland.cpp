cbb14955284f9d171cb72a655733574d6437eb0d
Anmol Narang <nanmol0406@gmail.com>
2022-05-16 23:07:56 +0530

    update

--- Graph/DFSIsland.cpp
+++ Graph/DFSIsland.cpp
@@ -4,7 +4,7 @@
 void DFS_Final(vector<int> adj[], int s, bool visited[])
 {
     visited[s] = true;
-    cout << s << " ";
+    // cout << s << " ";
     for (int u; adj[s])
     {
         if (visited == false)
@@ -13,16 +13,4 @@ void DFS_Final(vector<int> adj[], int s, bool visited[])
             DFS_Final(adj, u, visited);
         }
     }
-}
-void DFS(vector<int> adj, int v)
-{
-    vector<bool> vec(v, false);
-    // or
-    bool visited[v];
-    for (int i = 0; i < v; i++)
-        visited[i] = false;
-
-    for (int i = 0; i < v; i++)
-        if (visited[i] == false)
-            DFS_Final(adj, i, visited);
 }
