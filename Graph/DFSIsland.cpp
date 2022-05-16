0ef2aaed59519fed1e92444a74da6c1a7d3fc2ae
Anmol Narang <nanmol0406@gmail.com>
2022-05-16 23:08:16 +0530

    upd

--- Graph/DFSIsland.cpp
+++ Graph/DFSIsland.cpp
@@ -14,3 +14,18 @@ void DFS_Final(vector<int> adj[], int s, bool visited[])
         }
     }
 }
+int DFS(vector<int> adj, int v)
+{
+    int count = 0;
+    vector<bool> vec(v, false);
+    // or
+    bool visited[v];
+    for (int i = 0; i < v; i++)
+        visited[i] = false;
+
+    for (int i = 0; i < v; i++)
+        if (visited[i] == false)
+            DFS_Final(adj, i, visited);
+    count++;
+    return count;
+}
