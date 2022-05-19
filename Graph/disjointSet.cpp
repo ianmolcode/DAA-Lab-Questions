6fb907c82ad606d2a557efa30ff04d826af46f44
Anmol Narang <nanmol0406@gmail.com>
2022-05-19 12:56:16 +0530

    update

---
+++ Graph/disjointSet.cpp
@@ -0,0 +1,12 @@
+// detect cycle in an undirected graph using disjoint set
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int E;
+    int V;
+    cin << E << V;
+    dsuf.resize(V, -1);
+    vector<pair<int, int>>; // adjacency list
+}
